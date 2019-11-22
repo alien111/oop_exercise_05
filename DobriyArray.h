067a85c19c71682a96892db730001d481c3488de
alien111 <shoroxoff.al@yandex.ru>
2019-11-22 17:58:11 +0300

    Add files via upload

---
+++ DobriyArray.h
@@ -0,0 +1,178 @@
+#ifndef D_CONTAINERS_DOBRIYARRAY_H_
+#define D_CONTAINERS_DOBRIYARRAY_H_ 1
+
+#include <iterator>
+#include <memory>
+#include <cstddef>
+#include "vertex.h"
+#include "rectangle.h"
+
+namespace containers { 
+
+template<class T>
+class DobriyArray {
+
+public:
+	DobriyArray();
+	DobriyArray(int sz);
+
+	struct forward_iterator {
+		using value_type = T;
+    	using reference = T&;
+    	using pointer = T*;
+    	using difference_type = ptrdiff_t;
+    	using iterator_category = std::forward_iterator_tag;
+
+    	forward_iterator(T *ptr);
+    	forward_iterator() = default;
+    	T& operator*();
+
+    	forward_iterator& operator++();
+
+    	bool operator== (const forward_iterator& o) const;
+    	bool operator!= (const forward_iterator& o) const;
+
+    private:
+    	T *p;
+    	friend DobriyArray;
+
+	};
+
+	forward_iterator begin();
+	forward_iterator end();
+
+	T& operator[](int index);
+	void reSize(int newSize);
+	int getSize();
+	int getUsed();
+	forward_iterator insert(forward_iterator it, T object);
+	void erase(forward_iterator it);
+
+private:
+
+	std::unique_ptr<T[]> data;
+	int size;
+	int used;
+
+};
+
+template<class T>
+DobriyArray<T>::DobriyArray() {
+	
+	data = nullptr;
+	size = 0;
+	used = 0;
+
+}
+
+template<class T>
+void DobriyArray<T>::reSize(int newSize) {
+	if (size == newSize) return;
+	std::unique_ptr<T[]> resizing = std::unique_ptr<T[]>(new T[newSize]);
+
+	for (int i = 0; i < std::min(used, newSize); i++) {
+		resizing[i] = data[i];
+	} 
+	data = std::move(resizing);
+}
+
+template<class T>
+DobriyArray<T>::DobriyArray(int sz) {
+
+	data = std::unique_ptr<T[]>(new T[sz]);
+	size = sz;
+	used = 0;
+
+}
+
+
+template<class T>
+DobriyArray<T>::forward_iterator::forward_iterator(T *ptr) {
+	p = ptr;
+}
+
+template<class T>
+T& DobriyArray<T>::forward_iterator::operator*() {
+	return *p;
+}
+
+template<class T>
+typename DobriyArray<T>::forward_iterator& DobriyArray<T>::forward_iterator::operator++() {
+	++p;
+	return *this; 
+}
+
+template<class T>
+bool DobriyArray<T>::forward_iterator::operator== (const forward_iterator& o) const {
+	return p == o.p;
+}
+
+
+template<class T>
+bool DobriyArray<T>::forward_iterator::operator!= (const forward_iterator& o) const {
+	return p != o.p;
+}
+
+template<class T>
+typename DobriyArray<T>::forward_iterator DobriyArray<T>::begin() {
+	return &data[0];
+}
+
+template<class T>
+typename DobriyArray<T>::forward_iterator DobriyArray<T>::end() {
+	return &data[size];
+}
+
+template<class T>
+T& DobriyArray<T>::operator[](int index) {
+	if (index > size - 1) throw std::logic_error("index is out of range!\n");
+	T& result = data[index];
+	return result;
+}
+
+template<class T>
+int DobriyArray<T>::getSize() {
+	return size;
+}
+
+template<class T>
+int DobriyArray<T>::getUsed() {
+	return used;
+}
+
+template<class T>
+typename DobriyArray<T>::forward_iterator DobriyArray<T>::insert(forward_iterator it, T object) {
+	for (int i = 0; i < size; i++) {
+		if (it == &data[i]) {
+			if (used == size) reSize(size++);
+			for (int j = used - 1; j >= 0; j--) {
+				data[j + 1] = data[j];
+			}
+			data[i] = object;
+			used++;
+			return &data[i];
+		}
+	}
+
+	throw std::logic_error("Place doesn't exist!\n");
+}
+
+template<class T>
+void DobriyArray<T>::erase(forward_iterator it) {
+	for (int i = 0; i < size; i++) {
+		if (it == &data[i]) {
+			for (int j = i; j < size; j++) {
+				data[j] = data[j + 1];
+			}
+			used--;
+			return;
+		}
+	}
+
+	throw std::logic_error("Place doesn't exist!\n");
+} 
+
+}
+
+
+#endif
---
+++ Source.cpp
@@ -0,0 +1,144 @@
+#include <algorithm>
+#include <iostream>
+
+#include "containers/DobriyArray.h"
+
+void menu() {
+	std::cout << "0 : EXIT\n";
+	std::cout << "1 : GO THROUGH VECTOR WITH ITERATOR AND INPUT DATA\n";
+	std::cout << "2 : GET ITEM CENTER BY INDEX\n";
+	std::cout << "3 : GET AMOUNT OF OBJECTS WITH SQUARE LESS THAN...\n";
+	std::cout << "4 : GO THROUGH VECTOR WITH ITERATOR AND SHOW EVERY STEP\n";
+	std::cout << "5 : GO THROUGH VECTOR WITH ITERATOR AND INSERT A NEW OBJECT BEFORE THIS ONE\n";
+	std::cout << "6 : GO THROUGH VECTOR WITH ITERATOR AND ERASE THIS OBJECT\n";
+	std::cout << "> ";
+}
+
+int main() {
+
+
+	int cmd;
+	bool doWeHaveAVector = false;
+
+	std::cout << "Enter size of your vector : ";
+	int size;
+	std::cin >> size;
+
+	containers::DobriyArray< rectangle<int> > Vector(size);
+
+	while (true) {
+		menu();
+		std::cin >> cmd;
+		if (cmd == 0) return 0;
+		else if (cmd == 1) {
+			for (int i = 0; i < Vector.getSize(); i++) {
+				std::cout << "Enter vertices : \n";
+				rectangle<int> rect(std::cin);
+				Vector[i] = rect;
+			} 
+		} else if (cmd == 2) {
+			std::cout << "Enter index : ";
+			int index;
+			std::cin >> index;
+			std::cout << Vector[index].center();
+		} else if (cmd == 3) {
+			int res = 0;
+			std::cout << "Enter your square : ";
+			double square;
+			std::cin >> square;
+
+			int cmdcmd;
+			std::cout << "Do you want to use std::count_if? : 1 - yes; 0 - no; : ";
+			std::cin >> cmdcmd;
+
+			if (cmdcmd == 1) res = std::count_if(Vector.begin(), Vector.end(), [square](rectangle<int> i) {return i.area() < square;});
+			else {
+				auto it = Vector.begin();
+				auto end = Vector.end();
+
+				while (it != end) {
+					if ((*it).area() < square) res++;
+					++it;
+				}
+			}
+
+			std::cout << "Amount is " << res << '\n';
+		} else if (cmd == 4) {
+			int cmdcmd;
+			std::cout << "Do you want to use std::for_each? : 1 - yes; 0 - no; : ";
+			std::cin >> cmdcmd;
+
+			if (cmdcmd == 1) std::for_each(Vector.begin(), Vector.end(), [](rectangle<int> i) -> void{i.print();});
+			else {
+				auto it = Vector.begin();
+				auto end = Vector.end();
+
+				int n = 0; 
+
+				while (it != end) {
+					std::cout << "___OBJECT_" << n << "__\n";
+					std::cout << *it;
+					++it;
+					n++;
+				}
+
+			}
+
+		} else if (cmd == 5) {
+			std::cout << "Enter vertices of object you want to delete : "; 
+			rectangle<int> toDelete(std::cin);
+
+			std::cout << "Enter vertices of object you want to insert : "; 
+			rectangle<int> toInsert(std::cin);
+			
+			auto it = Vector.begin();
+			auto end = Vector.end();
+
+			while (it != end) {
+				if (*it == toDelete) {
+					Vector.insert(it, toInsert);
+				}
+				++it;
+			}
+
+			it = Vector.begin();
+
+			std::cout << "Now vector is like : \n";
+			int n = 0;
+
+			while (it != end) {
+				std::cout << "___OBJECT_" << n << "__\n";
+				std::cout << *it;
+				++it;
+				n++;
+			}
+		} else if (cmd == 6) {
+			std::cout << "Enter vertices of object you want to erase : "; 
+			rectangle<int> toDelete(std::cin);
+
+			auto it = Vector.begin();
+			auto end = Vector.end();
+
+			while (it != end) {
+				if (*it == toDelete) {
+					Vector.erase(it);
+				}
+				++it;
+			}
+
+			it = Vector.begin();
+
+			std::cout << "Now vector is like : \n";
+			int n = 0;
+
+			while (it != end) {
+				std::cout << "___OBJECT_" << n << "__\n";
+				std::cout << *it;
+				++it;
+				n++;
+			}
+		}  
+	}
+ 	
+
+}
